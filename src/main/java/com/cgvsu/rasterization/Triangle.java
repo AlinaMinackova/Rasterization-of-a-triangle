2b8bfb26f7f6a7ab524ca30ae23df1bef55a15d0
AlinaMinakova <94383135+AlinaMinackova@users.noreply.github.com>
2024-10-02 20:35:34 +0300

    commit

---
+++ src/main/java/com/cgvsu/rasterization/Triangle.java
@@ -0,0 +1,123 @@
+package com.cgvsu.rasterization;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.paint.Color;
+
+import java.util.Arrays;
+
+public class Triangle {
+
+    private int[] coorX;
+    private int[] coorY;
+    private Color color0;
+    private Color color1;
+    private Color color2;
+
+    public Triangle(int[] coorX, int[] coorY, Color color0, Color color1, Color color2) {
+        this.coorX = coorX;
+        this.coorY = coorY;
+        this.color0 = color0;
+        this.color1 = color1;
+        this.color2 = color2;
+    }
+
+    public int[] barycentricCoordinates(int x, int y){
+
+
+        double alfa = (double) ((coorY[1] - coorY[2]) * (x - coorX[2]) + (coorX[2] - coorX[1]) * (y - coorY[2])) /
+                (double) ((coorY[1] - coorY[2]) * (coorX[0] - coorX[2]) + (coorX[2] - coorX[1]) * (coorY[0] - coorY[2]));
+
+        double betta = (double) ((coorY[2] - coorY[0]) * (x - coorX[2]) + (coorX[0] - coorX[2]) * (y - coorY[2])) /
+                (double) ((coorY[1] - coorY[2]) * (coorX[0] - coorX[2]) + (coorX[2] - coorX[1]) * (coorY[0] - coorY[2]));
+
+        double gamma = 1 - alfa - betta;
+
+        int r = Math.min(255, (int) Math.abs(color0.getRed()*255 * alfa + color1.getRed()*255 * betta + color2.getRed()*255 * gamma));
+        int g = Math.min(255, (int) Math.abs(color0.getGreen()*255 * alfa + color1.getGreen()*255 * betta + color2.getGreen()*255 * gamma));
+        int b = Math.min(255, (int) Math.abs(color0.getBlue()*255 * alfa + color1.getBlue()*255 * betta + color2.getBlue()*255 * gamma));
+
+        return new int[] {r, g, b};
+    }
+
+
+    public void draw(GraphicsContext graphicsContext) {
+        graphicsContext.clearRect(0, 0, 800, 600);
+        final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
+
+        //сортируем вершины
+        if(coorY[0] > coorY[1]){
+            int termY = coorY[0];
+            coorY[0] = coorY[1];
+            coorY[1] = termY;
+            int termX = coorX[0];
+            coorX[0] = coorX[1];
+            coorX[1] = termX;
+            Color color = color0;
+            color0 = color1;
+            color1 = color;
+        }
+        if(coorY[0] > coorY[2]){
+            int term = coorY[0];
+            coorY[0] = coorY[2];
+            coorY[2] = term;
+            int termX = coorX[0];
+            coorX[0] = coorX[2];
+            coorX[2] = termX;
+            Color color = color0;
+            color0 = color2;
+            color2 = color;
+        }
+        if(coorY[1] > coorY[2]){
+            int term = coorY[1];
+            coorY[1] = coorY[2];
+            coorY[2] = term;
+            int termX = coorX[1];
+            coorX[1] = coorX[2];
+            coorX[2] = termX;
+            Color color = color1;
+            color1 = color2;
+            color2 = color;
+        }
+
+        if(coorY[0] == coorY[1] && coorY[1] == coorY[2]){
+            for(int x = coorX[0]; x <= coorX[2]; x++){
+                int[] rgb = barycentricCoordinates(x, coorY[0]);
+                pixelWriter.setColor(x, coorY[0], Color.rgb(rgb[0], rgb[1], rgb[2]));
+            }
+        }
+        if(coorY[0] != coorY[1]){
+            for (int y = coorY[0]; y <= coorY[1]; y++) {
+                //находим x
+                int xl = ((coorX[1] - coorX[0]) * (y - coorY[0]) / (coorY[1] - coorY[0])) + coorX[0];
+                int xr = ((coorX[2] - coorX[0]) * (y - coorY[0]) / (coorY[2] - coorY[0])) + coorX[0];
+                if (xl > xr) { // проверяем границы
+                    int tempX = xl;
+                    xl = xr;
+                    xr = tempX;
+                }
+                for (int x = xl; x <= xr; x++) {
+                    int[] rgb = barycentricCoordinates(x, y);
+                    pixelWriter.setColor(x, y, Color.rgb(rgb[0], rgb[1], rgb[2]));
+                }
+            }
+        }
+        if (coorY[1] != coorY[2]){
+            for (int y = coorY[1]; y <= coorY[2]; y++) {
+                //находим x
+                int xl = ((coorX[1] - coorX[2]) * (y - coorY[2]) / (coorY[1] - coorY[2])) + coorX[2];
+                int xr = ((coorX[0] - coorX[2]) * (y - coorY[2]) / (coorY[0] - coorY[2])) + coorX[2];
+                if (xl > xr) {
+                    int tempX = xl;
+                    xl = xr;
+                    xr = tempX;
+                }
+                for (int x = xl; x <= xr; x++) {
+                    int[] rgb = barycentricCoordinates(x, y);
+                    pixelWriter.setColor(x, y, Color.rgb(rgb[0], rgb[1], rgb[2]));
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/cgvsu/rasterizationfxapp/RasterizationController.java
@@ -0,0 +1,57 @@
+package com.cgvsu.rasterizationfxapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import com.cgvsu.rasterization.*;
+import javafx.scene.paint.Color;
+
+public class RasterizationController {
+
+    @FXML
+    AnchorPane anchorPane;
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private void initialize() {
+        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
+        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
+
+//        new Triangle(new int[]{60, 200, 10}, new int[]{10, 100, 60}, Color.BISQUE,
+//                Color.PALEGOLDENROD, Color.AQUA).draw(canvas.getGraphicsContext2D());
+//
+//        new Triangle(new int[]{200, 200, 300}, new int[]{50, 100, 50}, Color.NAVAJOWHITE,
+//                Color.RED, Color.MAGENTA).draw(canvas.getGraphicsContext2D());
+//
+//
+//        new Triangle(new int[]{60, 200, 300}, new int[]{300, 200, 300}, Color.rgb(255, 255, 255),
+//                Color.rgb(0, 0, 255), Color.rgb(255, 0, 0)).draw(canvas.getGraphicsContext2D());
+//
+//        new Triangle(new int[]{600, 550, 500}, new int[]{250, 200, 250}, Color.VIOLET,
+//                Color.BLACK, Color.WHITE).draw(canvas.getGraphicsContext2D());
+
+
+        canvas.setOnMouseClicked(event -> {
+            switch (event.getButton()) {
+                case PRIMARY -> handlePrimaryClick(event);
+            }
+        });
+    }
+
+//    private void handlePrimaryClick (MouseEvent event){
+//        new Triangle(new int[]{100, (int) event.getX(), 600}, new int[]{250, (int) event.getY(), 250},  Color.GREEN,
+//                Color.RED, Color.BLUE).draw(canvas.getGraphicsContext2D());
+//        }
+
+    private void handlePrimaryClick (MouseEvent event){
+        new Triangle(new int[]{400, (int) event.getX(), 400}, new int[]{100, (int) event.getY(), 400}, Color.GREEN,
+                Color.RED, Color.BLUE).draw(canvas.getGraphicsContext2D());
+    }
+
+
+
+}
